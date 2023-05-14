12cef4d7cd5ee9085e7ceb4a5bfbc40989644252
vikas <vikas11shrm@gmail.com>
2023-05-14 13:56:53 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { coreComponents } from './common-component';
+import { rootPages } from './pages';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...coreComponents,
+    ...rootPages
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductComponent } from './product.component';
+import { Product1Component } from './product1/product1.component';
+import { Product2Component } from './product2/product2.component';
+import { Product3Component } from './product3/product3.component';
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    Product1Component,
+    Product2Component,
+    Product3Component
+  ],
+  imports: [
+    CommonModule,
+    ProductRoutingModule
+  ]
+})
+export class ProductModule { }
